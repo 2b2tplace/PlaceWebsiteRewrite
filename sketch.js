7efe82c1802795f357bc5616946340ec6fc39620
crxyne <craynemail@gmail.com>
2025-12-28 17:06:18 +0100

    awdsfg

--- sketch.js
+++ sketch.js
@@ -9,7 +9,7 @@ let uiElements = {}
 let tilesToDraw = [];
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+	createCanvas(windowWidth, windowHeight, C2D);
 	camera.on();
 	camera.x = 0;
 	camera.y = 0;
