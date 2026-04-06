9d60158898f159a7c98ed4c85ff3d2ecb765ff2a
austin <austinxthomas@gmail.com>
2026-04-07 00:40:48 +0100

    ehh this is fine

--- sketch.js
+++ sketch.js
@@ -655,8 +655,7 @@ function draw() {
 	const halfHeight = height / 2;
 
 	if (cameraVel >= 0) {
-		lod = Math.floor(-Math.log2(camera.zoom / (Math.pow(camera.zoom, -0.25) * 1.5)));
-		// lod = Math.floor(-Math.log2(camera.zoom ** 3 / 1));
+		lod = Math.floor(-Math.log2(camera.zoom / (Math.pow(camera.zoom, -0.1) * 1.5)));
 		lod = Math.max(0, Math.min(10, lod));
 	}
 
